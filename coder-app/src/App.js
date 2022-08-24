310aa2ba513c8d49cbfb8175091ffe961ef39179
Dante <dantealioto@yahoo.it>
2022-08-24 19:17:53 -0300

    Subiendo archivos

--- coder-app/src/App.js
+++ coder-app/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let imagen = "React-Syntonize.png" ;
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+
+        <div className='col-md-4 offset-md-2'> 
+        <h1>Hola mundo!</h1>
+        <p>Este es un parrafo es mi primera pagina de react.</p>
+        </div>
+
+        <div className='col-md-4'> 
+        <p>Este es un segundo parrafo de mi primera pagina de react.</p>
+        </div>
+
+        <div className='col-md-4'> 
+        <img src = {imagen} width= "1000" alt= "logo_react"/>
+        </div>
+
+      </div>
     </div>
   );
 }
